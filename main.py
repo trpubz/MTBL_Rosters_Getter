961918abe700e7e3c4de6a4f1abaadf4d61b7564
trpubz <tpubz@icloud.com>
2023-05-18 17:04:39 -0700

    started modification to add lgmngrs.json
    refactored webdriver manager in DriverKit.py.  Storing the scraped .html into a temp .json file for lighter debugging.  TODO delete temp file when done.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from sources.DriverKit import DKDriverConfig
 from sources.TRPFrontOffice import Team
@@ -6,23 +7,40 @@ from sources.TRPFrontOffice import Team
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 
+# set global variables
+lgRostersBaseURL = "https://fantasy.espn.com/baseball/league/rosters?leagueId="
+
 rosters: list = list()
 teams: list = list()
 
-
+# function to open web driver and get rosters
 def TRPGetRosters(url: str):
     driver = DKDriverConfig()
     driver.get(url)
+    print("successfully navigated to " + url)
     driver.implicitly_wait(3)
-    rosterElements = driver.find_elements(By.CLASS_NAME, "jsx-1671979099.rosterTable")
+    rosterElementClassName = "jsx-1671979099.rosterTable"
+    rosterElements = driver.find_elements(By.CLASS_NAME, rosterElementClassName)
     for ros in rosterElements:
         rosters.append(ros.get_attribute("outerHTML"))
+    print("successfully pulled rosters in global variable")
+    # Save the list to a file
+    with open("tempRawRosters", 'w') as f:
+        json.dump(rosters, f)
+    
     driver.close()
 
 
-def TRPFillRosters():
+def TRPFillRosters(directory: str, lgMngrsFileName: str = "lgmngrs.json"):
+    # load a local .json file
+    managerKeys: dict
+    with open(os.path.join(directory, lgMngrsFileName), "r") as f:
+        managerKeys = json.load(f)["data"] # store only the data and not the schema
+        f.close()
+        print("successfully loaded " + lgMngrsFileName)
+    
     for ros in rosters:
-        soup = BeautifulSoup(ros, "lxml")
+        soup = BeautifulSoup(ros, "lxml") # lxml is faster than html.parser
         teamName = soup.find("span", class_="teamName truncate").text
         tm = Team(name=teamName)
         players = soup.find("tbody").find_all("tr")
@@ -49,8 +67,12 @@ def TRPJSONSave(directory: str):
 
 
 if __name__ == '__main__':
-    TRPGetRosters('https://fantasy.espn.com/baseball/league/rosters?leagueId=10998')
-    TRPFillRosters()
-    directory = "/Users/Shared/BaseballHQ/regseason/"
+    directory = "/Users/Shared/BaseballHQ/resources/extract"
+
+    lgID = "10998"
+    # retrieve rosters and store them in global rosters list
+    TRPGetRosters(lgRostersBaseURL + lgID)
+    TRPFillRosters(directory=directory)
+    
     TRPJSONSave(directory=directory)
     print(f"successfully saved rosters.json to {directory}")
