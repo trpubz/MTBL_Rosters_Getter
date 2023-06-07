d44692fb75211bb79e551c83010c1c03f7215450
trpubz <tpubz@icloud.com>
2023-06-07 15:34:04 -0700

    added WebDriverWait for roster table appearance.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sources.TRPFrontOffice import Team
 
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # set global variables
 lgRostersBaseURL = "https://fantasy.espn.com/baseball/league/rosters?leagueId="
@@ -21,28 +23,34 @@ def TRPGetRosters(url: str):
     driver.get(url)
     print("successfully navigated to " + url)
     driver.implicitly_wait(3)
-    rosterElementClassName: str = "rosterTable" # sometimes modified by host website
-    rosterElements = driver.find_elements(By.CLASS_NAME, rosterElementClassName)
+    rosterElementClassName: str = "rosterTable"  # sometimes modified by host website
+    # wait for the roster elements to load
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, rosterElementClassName)))
+        rosterElements = driver.find_elements(By.CLASS_NAME, rosterElementClassName)
+    except Exception as e:
+        exit(f"Could not locate Roster Tables.  Error: {e}")
+
     for ros in rosterElements:
         rosters.append(ros.get_attribute("outerHTML"))
-    print("successfully pulled rosters in global variable")
+    print("successfully pulled rosters in memory")
     # Save the list to a temp file
     with open("tempRawRosters.json", 'w') as f:
         json.dump(rosters, f)
 
-    driver.close()
+    driver.quit()
 
 
 def TRPFillRosters(path: str, lgMngrsFileName: str = "lgmngrs.json"):
-    # load a local .json file
+    # load a local .json file for the league managers
     managerKeys: list
     with open(os.path.join(path, lgMngrsFileName), "r") as f:
         managerKeys = json.load(f)["data"]  # store only the data and not the schema
         f.close()
-        print("successfully loaded " + lgMngrsFileName)
+        print("successfully loaded " + lgMngrsFileName + " for lgrstrs instantiation")
 
     # if the rosters list is empty, load it from the temp file
-    global rosters # declaring global here quiets the UnboundLocalError
+    global rosters  # declaring global here quiets the UnboundLocalError
     if len(rosters) == 0:
         with open("tempRawRosters.json", 'r') as f:
             rosters = json.load(f)
@@ -80,11 +88,12 @@ def TRPJSONSave(path: str, fileName: str = "lgrstrs.json") -> bool:
     with open(os.path.join(path, fileName), "w+") as f:
         json.dump(jsonTeams, f, indent=2)
         f.close()
-        print(f"successfully saved lgrstrs.json to {dirPath}")
+        print(f"successfully saved {fileName} to {dirPath}")
         return True
 
 
 def main():
+    print("\n---starting rosters getter---\n")
     # retrieve rosters and store them in global rosters list
     TRPGetRosters(lgRostersBaseURL + lgID)
     TRPFillRosters(path=dirPath)
@@ -93,6 +102,8 @@ def main():
     if goodSave:
         # delete the temp file
         os.remove("tempRawRosters.json")
+
+    print("\n---rosters getter complete---")
 
 
 if __name__ == '__main__':
